d18fde9ba78c49bda4f30e9b9bb797b77a05f6f3
calvinl-l-l <calvongchi@gmail.com>
2017-09-27 11:54:19 +1000

    backup: the evil mavlink delay issue came back

--- qav500/src/AP_interface.h
+++ qav500/src/AP_interface.h
@@ -46,6 +46,7 @@ struct IMU_data_t
 struct mavlink_data_t
 {
     //rc channels
+    int ch1_PPM;
     int ch1;
     int ch2;
     int ch3;
@@ -54,6 +55,7 @@ struct mavlink_data_t
     int ch6;
     int ch7;
     int ch8;
+    int ch9;
     int prev_ch5;
 
     //attitude in degree
